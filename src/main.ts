11e7491776b9b318d5d084f354953f81cb87c0c4
xxxx <xxxxemail>
2024-04-11 16:29:20 +0800

    feat: 使用vite-plugin-pages

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { setupLayouts } from 'virtual:generated-layouts'
+import generatedRoutes from '~pages'
 import App from './App.vue'
 import router from './router'
-
+const routes = setupLayouts(generatedRoutes)
 const app = createApp(App)
 //hellodfs
 app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [vue(), vueJsx(), VueDevTools(), Pages({ extensions: ['vue', 'md'] }), Layouts()],
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '~/': `${path.resolve(__dirname, 'src')}/`
     }
   }
 })
